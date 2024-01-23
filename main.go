dcdc6b5094c08275a1a83688f2c864afc97e0dc8
LucasFishwicas <luke-fishwick_@hotmail.com>
2024-01-22 19:51:11 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+
+func main() {
+    // Define a handler function for incoming requests
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w,"Welcome to tincan\nThe single-line CLI chat service")
+    })
+
+    // Start the server on port 8080
+    fmt.Println("Server listening on port 8080")
+    http.ListenAndServe(":8080", nil)
+}
