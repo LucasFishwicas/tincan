078972fac5ecf773404c4c365e1db06bbf1334ef
LucasFishwicas <luke-fishwick_@hotmail.com>
2024-01-25 18:25:16 -0500

    Setting up WebSocket  - unable to test WS using curl, unsure if its due to limitation with curl or my server

--- main.go
+++ main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+}
+
+
+func handler(w http.ResponseWriter,r *http.Request) {
+    _, err := upgrader.Upgrade(w,r,nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    fmt.Println("Successfully upgraded http to Websocket")
+}
+
 
 func main() {
 
     // Define a handler function for incoming requests
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w,"Welcome to tincan\nThe single-line CLI chat service")
+        fmt.Fprintf(w,"Welcome to tincan\nThe single-line CLI chat service\n")
+        fmt.Fprintf(w,r.Header.Get("Sec-WebSocket-Key"))
+        handler(w,r)
     })
     
 
@@ -17,3 +40,4 @@ func main() {
     fmt.Println("Server listening on port 8080")
     http.ListenAndServe(":8080", nil)
 }
+
