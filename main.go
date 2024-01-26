fd5279ab00027719fd1471649c905a5a4a8492d4
LucasFishwicas <luke-fishwick_@hotmail.com>
2024-01-25 21:02:17 -0500

    Check if request wants to upgrade to WS, if not handle regular HTTP request I installed websocat to test the websocket within the terminal - error while Reading (unexpected EOF)

--- main.go
+++ main.go
@@ -9,29 +9,73 @@ import (
 )
 
 
+
+
+// Set upgrader variable to access Upgrader method
 var upgrader = websocket.Upgrader{
     ReadBufferSize: 1024,
     WriteBufferSize: 1024,
 }
 
 
+
+
+// Handler function to handle requests
 func handler(w http.ResponseWriter,r *http.Request) {
-    _, err := upgrader.Upgrade(w,r,nil)
+    // Check if request wants to upgrade to Websocket
+    if r.Header.Get("Upgrade") == "websocket" {
+        wsHandler(w,r)
+    } else {
+        httpHandler()
+    }
+    
+}
+
+
+
+func wsHandler(w http.ResponseWriter,r *http.Request) {
+    // Upgrade to Websocket or print error and return
+    conn, err := upgrader.Upgrade(w,r,nil)
     if err != nil {
         log.Println(err)
         return
     }
 
+    // Print message to Server
     fmt.Println("Successfully upgraded http to Websocket")
+
+    // Eternally loop and check messages -causes error on ReadMessage
+    for {
+        messageType,_,err := conn.ReadMessage()
+        if err != nil {
+            log.Println("ReadMessage:",err)
+            return
+        }
+
+        if err := conn.WriteMessage(messageType,[]byte("Sent from WriteMessage?")); err != nil {
+            log.Println("WriteMessage:",err)
+            return
+        }
+    }
 }
 
 
-func main() {
 
+func httpHandler() {
+    // Handle Regular HTTP Request
+    fmt.Println("Not a websocket upgrade")
+}
+
+
+
+
+
+
+func main() {
     // Define a handler function for incoming requests
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w,"Welcome to tincan\nThe single-line CLI chat service\n")
-        fmt.Fprintf(w,r.Header.Get("Sec-WebSocket-Key"))
+        
         handler(w,r)
     })
     
