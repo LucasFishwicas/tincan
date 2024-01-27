5eb8ff5238bd92a55fa6ade70cd367674706eecd
LucasFishwicas <luke-fishwick_@hotmail.com>
2024-01-27 16:17:33 -0500

    Adding server-side user input for sending messages to client this has introduced a bug where the server closes immediately

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -11,11 +13,15 @@ import (
 
 
 
-// Set upgrader variable to access Upgrader method
-var upgrader = websocket.Upgrader{
-    ReadBufferSize: 1024,
-    WriteBufferSize: 1024,
-}
+var (
+    // Set upgrader variable to access Upgrader method
+    upgrader = websocket.Upgrader{
+        ReadBufferSize: 1024,
+        WriteBufferSize: 1024,
+    }
+    // Create channel for sending server-side input to client
+    inputChan = make(chan string)
+)
 
 
 
@@ -33,6 +39,22 @@ func handle(w http.ResponseWriter,r *http.Request) {
 
 
 
+func readInput() {
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        text := scanner.Text() // Get the current line of text
+        if text != "" {
+            inputChan <- text
+        }
+    }
+    if err := scanner.Err(); err != nil {
+        log.Println("Error reading input:", err)
+    }
+    close(inputChan)
+}
+
+
+
 func wsHandler(w http.ResponseWriter,r *http.Request) {
     // Upgrade to Websocket or print error and return
     conn, err := upgrader.Upgrade(w,r,nil)
@@ -45,11 +67,6 @@ func wsHandler(w http.ResponseWriter,r *http.Request) {
     // Notify server of successful upgrade
     fmt.Println("Successfully Upgraded http to Websocket")
 
-    
-    // ERROR ON CLIENT SIDE
-    // Receives HTTP 200 OK instead of HTTP 101 SWITCHING PROTOCOL
-
-
     // Send a welcome message  --  doesn't appear
     err = conn.WriteMessage(websocket.TextMessage, []byte("Hello from server!"))
     if err != nil {
@@ -57,20 +74,25 @@ func wsHandler(w http.ResponseWriter,r *http.Request) {
         return
     }
 
+    go readInput() 
 
     // Eternally loop and check messages - causes error on ReadMessage
-    /*for {
-        messageType,_,err := conn.ReadMessage()
-        if err != nil {
-            log.Println("Erro reading message:",err)
-            return
+    for {
+        select {
+            case input := <-inputChan: 
+            if err := conn.WriteMessage(websocket.TextMessage,[]byte(input)); err != nil {
+                log.Println("Error writing message:",err)
+                return
+            }
+        default:
+            messageType,_,err := conn.ReadMessage()
+            if err != nil {
+                log.Println("Error reading message:",err)
+                return
+            }
+            log.Println(messageType)
         }
-
-        if err := conn.WriteMessage(messageType,[]byte("Sent from WriteMessage?")); err != nil {
-            log.Println("WriteMessage:",err)
-            return
-        }
-    }*/
+    }
 }
 
 
@@ -88,11 +110,9 @@ func httpHandler() {
 func main() {
     // Define a handler function for incoming requests
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w,"Welcome to tincan\nThe single-line CLI chat service\n")
-        
-        // handle http request or ws upgrade request 
-        handle(w,r)
+        fmt.Fprintf(w,"Welcome to tincan\nThe single-line CLI chat service\n") 
     })
+    http.HandleFunc("/ws",wsHandler)
     
 
     // Start the server on port 8080
