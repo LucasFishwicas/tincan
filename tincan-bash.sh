8334953a0804c224163d9a9aa2f1aea5c1248f20
Sam-Fishwick <sam.fishwick@hotmail.com>
2024-02-02 16:49:55 +0000

    Making curl command in bash script verbose

--- tincan-bash.sh
+++ tincan-bash.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 echo "Container: Started docker container"
-curl "localhost:8080"
+curl -v "localhost:8080"
 read -p "send or receive? " response
 if [ $response = "send" ]; then
-    echo "send!"
     read -p "name: " name
-    curl -G "localhost:8080/http/send" --data-urlencode "message=enter" --data-urlencode "user=$name"
+    curl -v -G "localhost:8080/http/send" --data-urlencode "message=enter" --data-urlencode "user=$name"
     read -p "message: " message
     echo "name: $name, message: $message"
-    curl -G "localhost:8080/http/send" --data-urlencode "message=$message" --data-urlencode "user=$name"
+    curl -v -G "localhost:8080/http/send" --data-urlencode "message=$message" --data-urlencode "user=$name"
     while [[ $message != "exit" ]]; do
         read -p "message: " message
         echo "name: $name, message: $message"
-        curl -G "localhost:8080/http/send" --data-urlencode "message=$message" --data-urlencode "user=$name"
+        curl -v -G "localhost:8080/http/send" --data-urlencode "message=$message" --data-urlencode "user=$name"
     done
 elif [ $response = "receive" ]; then
-    echo "receive"
-    watch -n 2 curl "localhost:8080/http/receive"
+    watch -n 2 curl -v "localhost:8080/http/receive"
 else
     echo "unrecognised input"
 fi
